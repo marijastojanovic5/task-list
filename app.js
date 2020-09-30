578012846c8afbda67acf0fda198fa316f8a92bb
Marija <maria5stojanovic@gmail.com>
2020-09-29 22:16:17 -0400

    filter through tasks

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 /*jshint esversion: 6 */
-const form = document.querySelector('#task-form');
-const taskList = document.querySelector('.collection');
-const clearBtn = document.querySelector('.clear-tasks');
-const filter = document.querySelector('#filter');
-const taskInput = document.querySelector('#task');
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
 
 // Load all event listeners
 loadEventListeners();
@@ -11,27 +11,28 @@ loadEventListeners();
 // Load all event listeners
 function loadEventListeners() {
   // Add task event
-  form.addEventListener('submit', addTask);
-  taskList.addEventListener('click',deleteTask);
-  clearBtn.addEventListener('click',clearTasks);
+  form.addEventListener("submit", addTask);
+  taskList.addEventListener("click", deleteTask);
+  clearBtn.addEventListener("click", clearTasks);
+  filter.addEventListener("keyup", filterTasks);
 }
 
 // Add Task
 function addTask(e) {
-  if(taskInput.value === '') {
-    alert('Add a task');
+  if (taskInput.value === "") {
+    alert("Add a task");
   }
 
   // Create li element
-  const li = document.createElement('li');
+  const li = document.createElement("li");
   // Add class
-  li.className = 'collection-item';
+  li.className = "collection-item";
   // Create text node and append to li
   li.appendChild(document.createTextNode(taskInput.value));
   // Create new link element
-  const link = document.createElement('a');
+  const link = document.createElement("a");
   // Add class
-  link.className = 'delete-item secondary-content';
+  link.className = "delete-item secondary-content";
   // Add icon html
   link.innerHTML = '<i class="fas fa-times"></i>';
   // Append the link to li
@@ -41,24 +42,34 @@ function addTask(e) {
   taskList.appendChild(li);
 
   // Clear input
-  taskInput.value = '';
+  taskInput.value = "";
 
   e.preventDefault();
 }
 
 //delete task
-function deleteTask(e){
-  if(e.target.parentElement.classList.contains('delete-item')){
+function deleteTask(e) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
     e.target.parentElement.parentElement.remove();
-
   }
-
 }
 //clear tasks
-function clearTasks(){
-   while(taskList.firstChild){
-     taskList.removeChild(taskList.firstChild)
-   }
-   //other way
-   //taskList.innerHTML='';
+function clearTasks() {
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+  //other way
+  //taskList.innerHTML='';
+}
+//filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll(".collection-item").forEach(function (task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) !== -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
 }
